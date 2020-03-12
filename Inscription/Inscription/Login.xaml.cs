81bb451ba9cb9ffb1f53bab689e9325693d2b602
TraoreAminata99 <at193542@gmail.com>
2020-03-12 10:02:45 +0000

    first commit

---
+++ Inscription/Inscription/Login.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Inscription
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+
+
+            btn1.Clicked += Btn1_Clicked;
+            msg1.IsVisible = false;
+            
+        }
+
+        async  private void Btn1_Clicked(object sender, EventArgs e)
+        {
+            /*throw new NotImplementedException();*/
+            if ( edit1.Text == "laroche@gmail.com" && edit2.Text == "1234")
+            {
+                await Navigation.PushAsync(new Accueil());
+            }
+            else
+            {
+                msg1.Text = "Inscription impossible";
+                msg1.IsVisible = true;
+            }
+        }
+    }
+}
---
+++ Inscription/Inscription/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Inscription
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            btn.Clicked += Btn_Clicked;
+            message.IsVisible = false;
+
+
+        }
+
+
+        async private void Btn_Clicked(object sender, EventArgs e)
+        {
+
+            /*throw new NotImplementedException();*/
+
+            if (edit1.Text == "Laroche" && edit2.Text == "Elie" && edit3.Text == "laroche@gmail.com" && edit4.Text == "1234" && edit5.Text == "1234")
+            {
+                await Navigation.PushAsync(new Login());
+            }
+            else
+            {
+                message.Text = "Inscription impossible";
+                message.IsVisible = true;
+            }
+
+        }
+    }
+
+
+}
